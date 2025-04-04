d739901f29582d990d86dc8b842b2ca75b924a4e
Hudson Bui <hudsonbui369vn@gmail.com>
2025-04-04 19:32:29 +0700

    imp. Implement api view and logic for create new user

---
+++ app/user/urls.py
@@ -0,0 +1,12 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from user import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('sign-up/', views.CreateUserView.as_view(), name='sign-up'),
+]

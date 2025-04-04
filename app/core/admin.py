32e25bc0188d752e66c4b62d77b126ca3e0c916e
Hudson Bui <hudsonbui369vn@gmail.com>
2025-04-04 09:45:58 +0700

    imp. Implement creating new user for admin site, configure api documentation

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,7 @@
 """Django admin customizations."""
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -16,6 +17,32 @@ class UserAdmin(BaseUserAdmin):
         'is_first_time_user',
         'is_active',
     ]
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Personal Info'), {'fields': ('name', 'phone_number', 'address')}),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ('last_login',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', 'extrapretty'),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'phone_number',
+                'address',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
